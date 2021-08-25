b5fbb9b28f9d2836f8e861c6dae57fab0a9cc058
AaronBerndt <aaron_berndt@DragonSlayer.localdomain>
2021-08-25 18:24:44 -0500

    Fixed edit

--- api/updateCharacters.ts
+++ api/updateCharacters.ts
@@ -1,6 +1,7 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import { updateCollection } from "../utilities/MongoUtils";
 import microCors from "micro-cors";
+import { ObjectId } from "mongodb";
 
 const cors = microCors();
 
@@ -9,11 +10,48 @@ const handler = async (request: VercelRequest, response: VercelResponse) => {
     if (request.method === "OPTIONS") {
       return response.status(200).end();
     }
-    const { documents } = request.body.data;
-    const data = await updateCollection("characters", documents, {
+
+    const { documents, _id } = request.body.data;
+
+    const newCharacterData: any = {
+      _id,
       name: documents.name,
+      level: documents.level,
+      ancestry: documents.ancestry,
+      novicePath: documents.novicePath,
+      expertPath: documents.expertPath,
+      masterPath: documents.masterPath,
+      characteristics: [
+        ...documents.characteristics,
+      ].map(({ value, ...rest }) => ({ ...rest, value: Number(value) })),
+      talents: [],
+      spells: documents.spells,
+      traditions: documents.traditions,
+      items: {
+        weapons: documents.items.filter(
+          ({ itemType }) => itemType === "weapon"
+        ),
+        armor: documents.items.filter(({ itemType }) => itemType === "armor"),
+        otherItems: documents.items.filter(
+          ({ itemType }) => itemType === "basic"
+        ),
+        currency: documents.currency,
+      },
+      languages: [],
+      professions: [],
+      details: [],
+      characterState: {
+        damage: 0,
+        expended: [],
+        overrides: documents.overrides,
+        afflictions: [],
+      },
+    };
+
+    const data = await updateCollection("characters", newCharacterData, {
+      _id: new ObjectId(_id),
     });
-    response.status(200).send(data);
+    response.status(200).send("Test");
   } catch (e) {
     response.status(504).send(e);
   }

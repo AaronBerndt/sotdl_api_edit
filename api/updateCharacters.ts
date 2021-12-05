956d927115ea298706226d2a4c928ddf512dd485
AaronBerndt <aaron_berndt@DragonSlayer.localdomain>
2021-12-05 08:52:03 -0600

    Fixed updated character

--- api/updateCharacters.ts
+++ api/updateCharacters.ts
@@ -2,6 +2,7 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 import { updateCollection } from "../utilities/MongoUtils";
 import microCors from "micro-cors";
 import { ObjectId } from "mongodb";
+import { Item } from "./updateEquipCharacterGear";
 
 const cors = microCors();
 
@@ -28,30 +29,42 @@ const handler = async (request: VercelRequest, response: VercelResponse) => {
       spells: documents.spells,
       traditions: documents.traditions,
       items: {
-        weapons: documents.items.filter(
-          ({ itemType }) => itemType === "weapon"
-        ),
-        armor: documents.items.filter(({ itemType }) => itemType === "armor"),
-        otherItems: documents.items.filter(
-          ({ itemType }) => itemType === "basic"
-        ),
+        weapons: documents.items
+          .filter(({ itemType }) => itemType === "weapon")
+          .map(({ name }) => name),
+        armor: documents.items
+          .filter(({ itemType }) => itemType === "armor")
+          .map(({ name }) => name),
+        otherItems: documents.items
+          .filter(({ itemType }) => itemType === "basic")
+          .map(({ name }) => name),
         currency: documents.currency,
       },
       languages: [],
       professions: [],
       details: [],
+      choices: documents.choices,
       characterState: {
         damage: 0,
+        injured: false,
         expended: [],
         overrides: documents.overrides,
         afflictions: [],
+        temporaryEffects: [],
+        equipped: documents.items
+          .filter(({ itemType }) => itemType !== "basic")
+          .map((item: Item) => ({
+            _id: new ObjectId(),
+            name: item.name,
+            equipped: true,
+          })),
       },
     };
 
     const data = await updateCollection("characters", newCharacterData, {
       _id: new ObjectId(_id),
     });
-    response.status(200).send("Test");
+    response.status(200).send(`Updated character ${_id}`);
   } catch (e) {
     response.status(504).send(e);
   }
